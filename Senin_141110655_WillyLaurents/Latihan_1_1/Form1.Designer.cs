45793cc1be7eb71b8db8d27e3bf080a3e5ada957
willylaurents <willylaurents@gmail.com>
2016-09-29 22:33:23 +0700

    Latihan_1_1

---
+++ Senin_141110655_WillyLaurents/Latihan_1_1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Latihan_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.vScrollBarMin = new System.Windows.Forms.VScrollBar();
+            this.vScrollBarMax = new System.Windows.Forms.VScrollBar();
+            this.min = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(468, 279);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.max);
+            this.tabPage1.Controls.Add(this.min);
+            this.tabPage1.Controls.Add(this.vScrollBarMax);
+            this.tabPage1.Controls.Add(this.vScrollBarMin);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.ImageKey = "btn_accept_press.png";
+            this.tabPage1.Location = new System.Drawing.Point(4, 23);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(460, 252);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.ImageKey = "btn_cancel_press.png";
+            this.tabPage2.Location = new System.Drawing.Point(4, 23);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(460, 210);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "btn_accept_press.png");
+            this.imageList1.Images.SetKeyName(1, "btn_cancel_press.png");
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(27, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // vScrollBarMin
+            // 
+            this.vScrollBarMin.Location = new System.Drawing.Point(283, 35);
+            this.vScrollBarMin.Name = "vScrollBarMin";
+            this.vScrollBarMin.Size = new System.Drawing.Size(24, 134);
+            this.vScrollBarMin.TabIndex = 1;
+            this.vScrollBarMin.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBarMin_Scroll);
+            // 
+            // vScrollBarMax
+            // 
+            this.vScrollBarMax.Location = new System.Drawing.Point(375, 35);
+            this.vScrollBarMax.Name = "vScrollBarMax";
+            this.vScrollBarMax.Size = new System.Drawing.Size(23, 134);
+            this.vScrollBarMax.TabIndex = 2;
+            this.vScrollBarMax.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBarMax_Scroll);
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(280, 179);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(39, 13);
+            this.min.TabIndex = 3;
+            this.min.Text = "Min : 0";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Location = new System.Drawing.Point(372, 179);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(42, 13);
+            this.max.TabIndex = 4;
+            this.max.Text = "Max : 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 303);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.VScrollBar vScrollBarMax;
+        private System.Windows.Forms.VScrollBar vScrollBarMin;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Senin_141110655_WillyLaurents/Latihan_1_1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_1
+{
+    public partial class Form1 : Form
+    {
+        private String dateNow;
+        private String date;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateNow = DateTime.Now.Date.ToString("yyyy");
+            date = dateTimePicker1.Value.ToString("yyyy");
+
+            int old = int.Parse(dateNow) - int.Parse(date);
+            label3.Text = "Umur Anda : " + old.ToString();
+            int min = vScrollBarMin.Value;
+            int max = vScrollBarMax.Value;
+            if (old >= min && old <= max)
+            {
+                label4.Text = "cukup umur";
+            }
+            else
+            {
+                if (old >= max)
+                {
+                    label4.Text = "Umur terlalu tua";
+                }
+                else if (old <= min)
+                {
+                    label4.Text = "Tidak cukup umur";
+                }
+            }
+        }
+
+        private void vScrollBarMin_Scroll(object sender, ScrollEventArgs e)
+        {
+            min.Text = "Min :" + vScrollBarMin.Value.ToString();
+        }
+
+        private void vScrollBarMax_Scroll(object sender, ScrollEventArgs e)
+        {
+            max.Text = "Max :" + vScrollBarMax.Value.ToString();
+        }
+    }
+}
