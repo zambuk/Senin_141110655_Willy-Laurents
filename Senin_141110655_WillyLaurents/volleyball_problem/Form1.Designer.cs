c848472bb101a8dee8b8c1088c44b41bfb58e8af
willylaurents <willylaurents@gmail.com>
2016-11-24 21:48:39 +0700

    volleyball_problem

---
+++ Senin_141110655_WillyLaurents/volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score 2";
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(100, 22);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 20);
+            this.Txt1.TabIndex = 2;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(100, 53);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 20);
+            this.Txt2.TabIndex = 3;
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(100, 129);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(100, 20);
+            this.TxtHasil.TabIndex = 4;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(116, 89);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(75, 23);
+            this.BtnHitung.TabIndex = 5;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Button BtnHitung;
+    }
+}
+
---
+++ Senin_141110655_WillyLaurents/volleyball_problem/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+
+
+        static int Mod = 1000000007;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static long Volleyball(int a, int b)
+        {
+            if (a > b) return Volleyball(b, a);
+            if (b < 25) return 0;
+            if (b > 25 && b != a + 2) return 0;
+            if (b == 25 && b - a < 2) return 0;
+
+            int max = 25;
+            long[,] pascal = new long[max, max];
+
+            for (int i = 0; i < max; i++)
+            {
+                for (int j = 0; j < max; j++)
+                {
+                    if (i == 0 || j == 0)
+                        pascal[i, j] = 1;
+                    else
+                        pascal[i, j] = (pascal[i - 1, j] + pascal[i, j - 1]) % Mod;
+                }
+            }
+
+            if (b == 25)
+                return pascal[24, a];
+
+            return pascal[24, 24] % Mod * PowerMod(2, (a - 24), Mod) % Mod;
+        }
+
+        static long PowerMod(long a, long power, long n)
+        {
+            long mod = 1;
+
+            while (power != 0)
+            {
+                if ((power & 1) == 1)
+                    mod = mod * a % n;
+
+                a = (a * a) % n;
+                power >>= 1;
+            }
+
+            return mod;
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(Txt1.Text);
+            int b = Convert.ToInt32(Txt2.Text);
+            TxtHasil.Text = (Volleyball(a, b)).ToString();
+        }
+    }
+}
